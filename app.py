3f0250e27693579c73ccd4386d027aa4d9cd398d
Ankit Ranjan <me@ankit.io>
2014-04-10 11:58:50 -0700

    Made a rudimentary page for the server.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 #/usr/bin/env python
 
 from flask import Flask
+from flask import Response
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return Response('Hi, I\'m Ankit. Email me '
+    '<a href="mailto:me@ankit.io">here</a>. This page is under construction.')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=80)
